1978f4cbe4dfc529d4b901613d75a26acac4c01c
Jayson <kanekiken101017@gmail.com>
2023-08-11 17:57:13 +0800

    1st

---
+++ android/src/com/vvv/fishshooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.vvv.fishshooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vvv.fishshooter.FishShooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FishShooter(), config);
+	}
+}
